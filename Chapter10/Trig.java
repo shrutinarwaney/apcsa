8f36ef35d950181be2fb9f8d89017f644a4ca112
Shruti Narwaney <shruti.narwaney@gmail.com>
2020-09-07 12:17:17 -0600

    Add trig

---
+++ Chapter10/Trig.java
@@ -0,0 +1,12 @@
+public class Trig {
+
+	public static void main( String [] args ) {
+		
+		double sinx = Math.sin( 0.5236 );
+		double cosx = Math.cos( 0.5236 );
+		double sum = ( Math.pow( sinx, 2 ) + Math.pow( cosx, 2 ) );
+		System.out.println( "sine: " + sinx + " cosine: " + cosx + " cosine: " + sum );
+
+	}
+
+}
