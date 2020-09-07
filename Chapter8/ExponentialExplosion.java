143cd0a3c4c70bb01ae866b200f79d4ca952715e
Shruti Narwaney <shruti.narwaney@gmail.com>
2020-09-07 10:40:18 -0600

    Add ExponentialExplosion and break it

---
+++ Chapter8/ExponentialExplosion.java
@@ -0,0 +1,10 @@
+public class ExponentialExplosion {
+  
+    public static void main ( String[] args ) {
+      
+      double value = 10E+10000000;
+      System.out.println("e to the power value: " +  Math.exp( value ) );
+    
+    }
+  
+  }
