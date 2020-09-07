917c7015cc5aa3f92208201b4f990c04a05364a5
Shruti Narwaney <shruti.narwaney@gmail.com>
2020-09-07 11:52:19 -0600

    Add rainfall

---
+++ Chapter10/Rain.java
@@ -0,0 +1,17 @@
+public class Rain {
+
+	public static void main( String [] args ) {
+		
+		double april = 12;
+		double may = 14;
+		double june = 8;
+		double average = ( april + may + june ) / 3;
+		System.out.println( "Rainfall for April:\t" + april );
+		System.out.println( "Rainfall for May:\t" + may );
+		System.out.println( "Rainfall for June:\t" + june );
+		System.out.println( "Avwerage rainfall:\t" + average );
+		
+
+	}
+
+}
