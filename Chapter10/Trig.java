8c552583e800d76c2fa689bde4ca6d2fc67fe0cb
Shruti Narwaney <shruti.narwaney@gmail.com>
2020-09-07 12:17:46 -0600

    Add degrees

--- Chapter10/Trig.java
+++ Chapter10/Trig.java
@@ -2,8 +2,9 @@ public class Trig {
 
 	public static void main( String [] args ) {
 		
-		double sinx = Math.sin( 0.5236 );
-		double cosx = Math.cos( 0.5236 );
+		double rad = 30 * Math.PI / 180;
+		double sinx = Math.sin( rad );
+		double cosx = Math.cos( rad );
 		double sum = ( Math.pow( sinx, 2 ) + Math.pow( cosx, 2 ) );
 		System.out.println( "sine: " + sinx + " cosine: " + cosx + " cosine: " + sum );
 
