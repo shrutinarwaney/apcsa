031173a2f35e22e767513f9f6e4ed74d71a09627
Shruti Narwaney <shruti.narwaney@gmail.com>
2020-09-12 13:14:59 -0600

    Add Hello World String

---
+++ Chapter11/HelloWorldStr.java
@@ -0,0 +1,12 @@
+public class HelloWorldStr {
+
+	public static void main( String [] args ) {
+		
+		String str = "Hello World";
+		String secondVariable;
+		System.out.println( str );
+		System.out.println( secondVariable );
+
+	}
+
+}
