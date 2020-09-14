1966c0a5de2f58c97b1f11fdb629d2be1e8dc357
Shruti Narwaney <shruti.narwaney@gmail.com>
2020-09-14 11:13:07 -0600

    Add discount

---
+++ Chapter16/Discount.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Discount{
+
+	public static void main( String [] args ) {
+			
+		Scanner input = new Scanner( System.in );
+
+		System.out.print( "Enter amount of purchases: " );
+		int amount = input.nextInt();
+		if( amount > 1000 ) { System.out.println( "Discounted price: " + ( 0.9 * amount ) ); }
+		else { System.out.println( "No discount. Amount: " + amount ); }
+
+	}
+
+}
