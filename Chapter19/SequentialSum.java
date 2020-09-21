ec0f01497d0dc0527477f89e8fe0d3e1fa994a91
Shruti Narwaney <shruti.narwaney@gmail.com>
2020-09-21 10:47:20 -0600

    Add Sequential Sum

---
+++ Chapter19/SequentialSum.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SequentialSum {
+
+	public static void main( String [] args ) {
+		
+		Scanner input = new Scanner( System.in );
+
+		System.out.print( "Enter N: " );
+		int n = input.nextInt();
+
+		int formulaSum = ( n * ( n + 1 ) / 2 );
+
+		int sum = 0;
+
+		while( n > 0 ) {
+			sum = sum + n;
+			n--;
+		}
+
+		System.out.println( "Loop Sum = " + sum );
+		System.out.println( "Formula Sum = " + formulaSum );
+
+	}
+
+}
