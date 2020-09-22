d34142fdd2e6e7ff2194f1387c7ebb12a331d545
Shruti Narwaney <shruti.narwaney@gmail.com>
2020-09-22 12:24:54 -0600

    Get mean in Average and Standard Deviation Program

---
+++ Chapter20/AverageStandard.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class AverageStandard {
+
+	public static void main( String [] args ) {
+		
+		Scanner input = new Scanner( System.in );
+		System.out.print( "How many numbers: " );
+		// int num = input.nextInt();
+		double [] numArray = new double [ input.nextInt() ];
+		double [] diff = new double[ numArray.length ];
+
+		int x = 0;
+		while( x < numArray.length ) {
+			System.out.print( "What is your " + ( x + 1 ) + " number? " );
+			numArray[ x ] = input.nextDouble();
+			x++;
+		}
+
+		double mean = 0;
+		for( int i = 0; i < numArray.length; i++ ) {
+			mean += numArray[ i ];
+		}
+		mean = mean / numArray.length;
+		System.out.println( "Mean is " + mean );
+		double diffSum = 0;
+
+		for( int i = 0; i < numArray.length; i++ ) {
+			diff[ i ] = Math.pow( ( double )( numArray[ i ] - mean ), 2 );
+			diffSum += diff[ i ];
+		}
+
+		System.out.println( "Standard Deviation: " + diffSum / numArray.length );
+
+	}
+
+}
