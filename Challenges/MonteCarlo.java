f0abde3edc44507d1f75df675e92f7ff1651da65
Shruti Narwaney <shruti.narwaney@gmail.com>
2020-10-02 11:08:49 -0600

    Format code better

--- Challenges/MonteCarlo.java
+++ Challenges/MonteCarlo.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class MonteCarlo {
 
-	public static void main(String[] args) {
+	public static void main( String [] args v) {
 		
 		Scanner input = new Scanner( System.in );
 		Random rand = new Random();	
