ef10564b86d14704e569faf16c4802160cdb6cb4
Shruti Narwaney <shruti.narwaney@gmail.com>
2020-10-23 23:27:16 -0600

    Add exercise 1

---
+++ Chapter50/Box.java
@@ -0,0 +1,34 @@
+public class Box {
+	
+	private double width, height, length;
+
+	public Box( double width, double height, double length ) {
+		this.width = width;
+		this.height = height;
+		this.length = length;
+	}
+
+	public Box( double side ) {
+		this.width = side;
+		this.height = side;
+		this.length = side;
+	}
+
+	public void setWidth( double width ) { this.width = width; }
+	public double getWidth() { return width; }
+
+	public void setHeight( double height ) { this.height = height; }
+	public double getHeight() { return height; }
+
+	public void setLength( double length ) { this.length = length; }
+	public double getLength() { return length; }
+
+	public double volume() {
+		return length * width * height;
+	}
+
+	public double area() {
+		return 2 * ( ( width * length ) + ( height * length ) + ( height * width ) );
+	}
+
+}
