288ff76f643911c1229d9b9f5fc0a42dcd906f39
Shruti Narwaney <shruti.narwaney@gmail.com>
2020-11-12 13:13:48 -0700

    Add getters/setters

--- Challenges/TrinketBooks/Date.java
+++ Challenges/TrinketBooks/Date.java
@@ -18,6 +18,15 @@ public class Date {
 		day = 1;
 	}
 
+	public void setYear( int year ) { this.year = year; }
+	public int getYear() { return year; }
+
+	public void setMonth( int month ) { this.month = month; }
+	public int getMonth() { return month; }
+
+	public void setDay( int day ) { this.day = day; }
+	public int getDay() { return day; }
+
 	public String toString() {
         return month + "/" + day + "/" + year;
     }
